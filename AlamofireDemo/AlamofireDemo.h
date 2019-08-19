b00f47e20fdd6c6242f4312672dd95538e3ddd2f
Lim Thean Khoon <theankhoon@iscity.com.my>
2019-08-19 18:02:30 +0800

    Init new target.

---
+++ AlamofireDemo/AlamofireDemo.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireDemo.h
+//  AlamofireDemo
+//
+//  Created by Lim Thean Khoon on 19/08/2019.
+//  Copyright © 2019 Lim Thean Khoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireDemo.
+FOUNDATION_EXPORT double AlamofireDemoVersionNumber;
+
+//! Project version string for AlamofireDemo.
+FOUNDATION_EXPORT const unsigned char AlamofireDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireDemo/PublicHeader.h>
+
+
